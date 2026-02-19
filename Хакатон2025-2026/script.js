a318ea7e05d90d9c50318153f88c6cf2bc35a680
Vanni0824 <lukaninamargo@gmail.com>
2026-02-19 21:41:05 +0500

    Add files via upload

---
+++ Хакатон2025-2026/script.js
@@ -0,0 +1,26 @@
+
+document.getElementById('aa').addEventListener('click', function() {
+let image = document.getElementById('ff');
+if (image.style.display === 'none') {
+image.style.display = 'block';
+} else {
+image.style.display = 'none';
+}
+});
+
+document.getElementById('pp').addEventListener('click', function() {
+let image = document.getElementById('bb');
+if (image.style.display === 'none') {
+image.style.display = 'block';
+} else {
+image.style.display = 'none';
+}
+});
+document.getElementById('ss').addEventListener('click', function() {
+let image = document.getElementById('ll');
+if (image.style.display === 'none') {
+image.style.display = 'block';
+} else {
+image.style.display = 'none';
+}
+});
